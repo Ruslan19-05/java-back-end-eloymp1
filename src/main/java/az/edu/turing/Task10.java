ceed1d490fbfd75ba3dece5213153baee9affe65
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-11-05 11:20:35 +0400

    some changes

--- src/main/java/az/edu/turing/Task10.java
+++ src/main/java/az/edu/turing/Task10.java
@@ -1,4 +1,25 @@
 package az.edu.turing;
 
+import java.util.Scanner;
+
 public class Task10 {
+
+    public static void main(String[] args) {
+
+        //Buratino və bank
+        System.out.println("Bnkdakı pulu daxil edin:");
+        Scanner sc = new Scanner(System.in);
+        int S = sc.nextInt();
+        int n = 1;
+        if (2 <= S && S <=100000000) {
+            for(n=1;n>=1;n++) {
+                n=S-(S-n);
+                S=S*2+n;
+                n++;
+            }
+            System.out.print("Buritononun yaşı: " + n);
+        } else {
+            System.out.print("Bele mebleğ yoxdur!");
+        }
+    }
 }
